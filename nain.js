8355efd6fa828c79da3ddfa10930fa608b3d09f7
lexcorps <146864005+lexcorps@users.noreply.github.com>
2023-11-13 16:04:45 +0100

    Add files via upload

---
+++ nain.js
@@ -0,0 +1,11 @@
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
